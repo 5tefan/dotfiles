de9c67f691e1116191c7f45223d69076049c9ba1
Stefan Codrescu <ssmmcc1@gmail.com>
2021-07-07 10:17:17 -0600

    zsh-common: unset share history

--- zsh-common.zsh
+++ zsh-common.zsh
@@ -6,16 +6,19 @@ alias .....="cd ../../../.."
 export HISTFILE="$HOME/.zsh_history"
 export HISTSIZE=9999999
 export SAVEHIST=$HISTSIZE
-setopt inc_append_history
-setopt hist_ignore_all_dups
-setopt hist_ignore_space
+
+unsetopt SHARE_HISTORY
+setopt INC_APPEND_HISTORY
+setopt HIST_FIND_NO_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_REDUCE_BLANKS
 
 export WWW_HOME="https://duckduckgo.com/lite"
 export EDITOR="vim"
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
+alias rg='rg --color=always'
 
 source $HOME/.z.sh
-
 
